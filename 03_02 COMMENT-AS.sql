7c9f10912a87c7b215660a5f047d86f81f4ae7bf
Mr. Gong <gongliangtju@gmail.com>
2022-09-05 16:28:22 -0500

    02-03 files

---
+++ 03_02 COMMENT-AS.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Countries
+
+SELECT ID, CountryName FROM Countries
+
+SELECT 
+      ID
+-- this is a comment
+FROM Countries
+
+SELECT 
+      ID AS 'Identifier'
+    , CountryName AS 'Name'
+FROM Countries
---
+++ 03_05 STRING_SPLIT-FORMAT.sql
@@ -0,0 +1,3 @@
+SELECT * FROM STRING_SPLIT('United States of America', ' ')
+
+SELECT FORMAT(ID, 'N') FROM  Countries
